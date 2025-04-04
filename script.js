54b88c005ceee8890e1db60ae148f32e87022d29
Ayush Dwivedi <dwivediayush028@gmail.com>
2025-04-04 15:24:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-//your JS code here. If required.
+function submitForm(event) {
+    event.preventDefault(); 
+
+    let age = document.querySelector("#age").value.trim();
+    let name = document.querySelector("#name").value.trim();
+
+    if (age === '' || name === '') {
+        alert("Please enter valid details.");
+        return; 
+    }
+
+    age = parseInt(age);
+
+    let ageCheck = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (age >= 18) {
+                resolve(`Welcome, ${name}. You can vote.`);
+            } else {
+                reject(`Oh sorry ${name}. You aren't old enough.`);
+            }
+        }, 4000);
+    });
+
+    ageCheck
+        .then(message => {
+            alert(message);
+            form.submit(); 
+        })
+        .catch(error => alert(error));
+}
